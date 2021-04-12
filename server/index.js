a92aeda26df500ef8727c778e2ae95ed15ee933c
vov62 <avivovgen1@gmail.com>
2021-04-12 14:12:00 +0300

    update json

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ dbConnection.on('error', () => { console.log('dbConnection error'); })
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../app-client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../app-client/build', 'index.html'));
